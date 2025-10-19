133b33ce0e71ceff811401289ffb2301fac53a91
GiltMonster <lucassantoslukas2@gmail.com>
2025-10-19 06:48:56 -0300

    feat ✨: Adicionar funcionalidades de gerenciamento de veterinários, incluindo busca, edição e exclusão

--- back_end/app/Http/Controllers/VetController.php
+++ back_end/app/Http/Controllers/VetController.php
@@ -2,9 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Vet;
 use Illuminate\Http\Request;
 
 class VetController extends Controller
 {
-    //
+    function getVets()
+    {
+        $vets = Vet::where('type', 'vet')->get();
+        return response()->json($vets, 200);
+    }
+
+    function getVetById($id) {
+        $vet = Vet::where('type', 'vet')->find($id);
+        if (!$vet) {
+            return response()->json(['message' => 'Veterinário não encontrado.'], 404);
+        }
+
+        return response()->json($vet, 200);
+
+    }
+
+    public function editarVet(Request $request)
+    {
+        $newVetData = $request;
+        $vet = Vet::find($request->id);
+
+        if (!$vet) {
+            return response()->json(['message' => 'Veterinário não encontrado.'], 404);
+        }
+
+        if ($request->filled('password')){
+            $vet->password = bcrypt($request->password);
+            if ($vet->password === $newVetData->password) {
+                return response()->json(['message' => 'A nova senha não pode ser igual à senha atual.'], 400);
+            }
+        }
+
+        if ($vet == $newVetData) {
+            return response()->json(['message' => 'Nenhuma alteração detectada nos dados do veterinário.'], 200);
+        }
+
+        $vet->name = $newVetData->name;
+        $vet->email = $newVetData->email;
+        $vet->password = bcrypt($newVetData->password);
+        $vet->phone = $newVetData->phone;
+        $vet->cep = $newVetData->cep;
+        $vet->endereco = $newVetData->endereco;
+        $vet->cidade = $newVetData->cidade;
+        $vet->estado = $newVetData->estado;
+        $vet->bairro = $newVetData->bairro;
+        $vet->complemento = $newVetData->complemento;
+        $vet->cpf = $newVetData->cpf;
+        $vet->crmv = $newVetData->crmv;
+        $vet->pix = $newVetData->pix;
+        $vet->save();
+        return response()->json(['message' => 'Dados do veterinário atualizados com sucesso.'], 200);
+    }
+
+    function deletarVet($id)
+    {
+        $vet = Vet::find($id);
+
+        if (!$vet) {
+            return response()->json(['message' => 'Veterinário não encontrado.'], 404);
+        }
+
+        if($vet->type != 'vet') {
+            return response()->json(['message' => 'O usuário especificado não é um veterinário.'], 400);
+        }
+
+        $vet->delete();
+        return response()->json(['message' => 'Veterinário deletado com sucesso.'], 200);
+    }
 }
--- back_end/routes/api.php
+++ back_end/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PetController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VetController;
+use App\Models\User;
 
 Route::get('/ping', function() {
     return response()->json(['message' => 'API funcionando!']);
@@ -15,15 +19,19 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     // Protegidas por role
-    Route::middleware(['role:admin'])->get('/admin/dashboard', function () {
-        return response()->json(['msg' => 'Bem-vindo, Admin!'], 200);
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/adm/getVets', [VetController::class, 'getVets']);
+        Route::get('/adm/getVetById/{id}', [VetController::class, 'getVetById']);
+        Route::put('/adm/editarVet', [VetController::class, 'editarVet']);
+        Route::delete('/adm/deletarVet/{id}',  [VetController::class, 'deletarVet']);
+
     });
 
-    Route::middleware(['role:admin|vet'])->get('/vet/dashboard', function () {
+    Route::middleware(['role:admin|vet'])->get('/vet/teste', function () {
         return response()->json(['msg' => 'Bem-vindo, Veterinário!'], 200);
     });
 
-    Route::middleware(['role:admin|user'])->get('/user/dashboard', function () {
+    Route::middleware(['role:admin|user'])->get('/user/teste', function () {
         return response()->json(['msg' => 'Bem-vindo, Usuário!'], 200);
     });
 });
