3a6455e955226b0135a566038b7c36310a81058f
GiltMonster <lucassantoslukas2@gmail.com>
2025-10-23 08:11:58 -0300

    feat ✨: Organizar rotas de veterinários e clientes, removendo endpoints de teste e adicionando novas rotas para gerenciamento

--- back_end/routes/api.php
+++ back_end/routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\PetController;
 use App\Http\Controllers\VetController;
 
-Route::get('/ping', function() {
+Route::get('/ping', function () {
     return response()->json(['message' => 'API funcionando!']);
 });
 
@@ -23,8 +23,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/adm/getVetById/{id}', [VetController::class, 'getVetById']);
         Route::put('/adm/editarVet', [VetController::class, 'editarVet']);
         Route::delete('/adm/deletarVet/{id}',  [VetController::class, 'deletarVet']);
-
         Route::get('/adm/getClientes', [ClienteController::class, 'getClientes']);
+
         Route::get('/adm/getClienteById/{id}', [ClienteController::class, 'getClienteById']);
         Route::put('/adm/editarCliente', [ClienteController::class, 'editCliente']);
         Route::delete('/adm/deletarCliente/{id}', [ClienteController::class, 'deleteCliente']);
@@ -37,11 +37,20 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::delete('/adm/deletarPet/{id}', [PetController::class, 'deletarPet']);
     });
 
-    Route::middleware(['role:admin|vet'])->get('/vet/teste', function () {
-        return response()->json(['msg' => 'Bem-vindo, Veterinário!'], 200);
+    Route::middleware(['role:admin|vet'])->group(function () {
+        Route::get('/vet/getVetById/{id}', [VetController::class, 'getVetById']);
+        Route::put('/vet/editarVet', [VetController::class, 'editarVet']);
+        Route::post('/vet/registrarPet', [PetController::class, 'registrarPet']);
     });
 
-    Route::middleware(['role:admin|user'])->get('/user/teste', function () {
-        return response()->json(['msg' => 'Bem-vindo, Usuário!'], 200);
+    Route::middleware(['role:admin|user'])->group(function () {
+        Route::get('/cliente/getClienteById/{id}', [ClienteController::class, 'getClienteById']);
+        Route::put('/cliente/editarCliente', [ClienteController::class, 'editCliente']);
+
+        Route::get('/cliente/getPetById/{id}', [PetController::class, 'getPetById']);
+        Route::get('/cliente/getPetByUserId/{user_id}', [PetController::class, 'getPetByUserId']);
+        Route::post('/cliente/registrarPet', [PetController::class, 'registrarPet']);
+        Route::put('/cliente/editarPet', [PetController::class, 'editarPet']);
+        Route::delete('/cliente/deletarPet/{id}', [PetController::class, 'deletarPet']);
     });
 });
