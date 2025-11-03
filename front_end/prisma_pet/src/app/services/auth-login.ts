2d4a5c438dff180dc5f9426714a7b8fbec051397
GiltMonster <lucassantoslukas2@gmail.com>
2025-11-03 03:37:30 -0300

    feat✨: implementa lógica de inicialização no componente de login e aprimora o serviço de autenticação

--- front_end/prisma_pet/src/app/services/auth-login.ts
+++ front_end/prisma_pet/src/app/services/auth-login.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,14 @@ export class AuthLogin {
   baseUrl: string ='http://127.0.0.1:8000';
 
   login(email: string, password: string) {
-    return this.http.post(this.baseUrl + '/api/login', {email, password})
+    return this.http.post<{token: string, name: string}>(this.baseUrl + '/api/login', {email, password}).pipe(
+      tap((res) => {
+        if (res.token && res.name) {
+          localStorage.setItem('token', res.token);
+          localStorage.setItem('name', res.name);
+        }
+      })
+    );
   }
 
   logout () {
