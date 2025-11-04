e606630cb4478615bf2b619cfe5907bce8105c0c
GiltMonster <lucassantoslukas2@gmail.com>
2025-11-04 09:49:08 -0300

    feat✨: adiciona configuração de ambiente de desenvolvimento e substituições de arquivos

---
+++ front_end/prisma_pet/src/environments/environment.development.ts
@@ -0,0 +1,35 @@
+const end_point_base = 'http://127.0.0.1:8000/api';
+
+export const environment = {
+  production: false,
+
+  // --- auth ---
+  API_URL_AUTH_LOGIN: `${end_point_base}/login`,
+  API_URL_AUTH_LOGOUT: `${end_point_base}/logout`,
+  API_URL_AUTH_ME: `${end_point_base}/me`,
+  API_URL_AUTH_MY_TYPE: `${end_point_base}/myType`,
+  API_URL_AUTH_VERIFY_TOKEN: `${end_point_base}/verifyToken`,
+
+  // --- register ---
+  API_URL_REGISTER: `${end_point_base}/register`,
+
+  // --- admin ---
+  API_URL_ADMIN_VET_LIST: `${end_point_base}/adm/getVets`,
+  API_URL_ADMIN_VET_BY_ID: `${end_point_base}/adm/getVetById`,
+  API_URL_ADMIN_VET_UPDATE: `${end_point_base}/adm/editarVet`,
+  API_URL_ADMIN_VET_DELETE: `${end_point_base}/adm/deletarVet`,
+
+  API_URL_ADMIN_CLIENTE_LIST: `${end_point_base}/adm/getClientes`,
+  API_URL_ADMIN_CLIENTE_BY_ID: `${end_point_base}/adm/getClienteById`,
+  API_URL_ADMIN_CLIENTE_UPDATE: `${end_point_base}/adm/editarCliente`,
+  API_URL_ADMIN_CLIENTE_DELETE: `${end_point_base}/adm/deletarCliente`,
+
+  // --- veterinario ---
+
+  // --- tutor ---
+
+  // --- pet ---
+
+  // --- servicos ---
+
+};
