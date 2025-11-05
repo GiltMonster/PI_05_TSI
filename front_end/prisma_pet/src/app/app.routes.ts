5ae8235c48556d889bb924ab77410fc91a99793b
GiltMonster <lucassantoslukas2@gmail.com>
2025-11-05 20:26:05 -0300

    feat✨: organiza rotas em hierarquia para veterinários, tutores e administradores

--- front_end/prisma_pet/src/app/app.routes.ts
+++ front_end/prisma_pet/src/app/app.routes.ts
@@ -3,11 +3,51 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', loadComponent: () => import('./pages/login/login').then(m => m.Login) },
-  { path: 'vet', loadComponent: () => import('./pages/acesso-vet/acesso-vet').then(m => m.AcessoVet) },
-  { path: 'tutor', loadComponent: () => import('./pages/acesso-tutor/acesso-tutor').then(m => m.AcessoTutor) },
-  { path: 'admin', loadComponent: () => import('./pages/acesso-admin/acesso-admin').then(m => m.AcessoAdmin) },
+  {
+    path: 'vet',
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/acesso-vet/acesso-vet').then(m => m.AcessoVet)
+      }
+    ]
+  },
+  {
+    path: 'tutor',
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/acesso-tutor/acesso-tutor').then(m => m.AcessoTutor)
+      }
+    ]
+  },
+  {
+    path: 'admin',
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/acesso-admin/acesso-admin').then(m => m.AcessoAdmin)
+      },
+      {
+        path: 'tutores',
+        loadComponent: () => import('./pages/tutor/tutor').then(m => m.Tutor)
+      },
+      {
+        path: 'pets',
+        loadComponent: () => import('./pages/animais/animais').then(m => m.Animais)
+      },
+      {
+        path: 'veterinarios',
+        loadComponent: () => import('./pages/veterinarios/veterinarios').then(m => m.Veterinarios)
+      },
+      {
+        path: 'services',
+        loadComponent: () => import('./pages/servicos/servicos').then(m => m.Servicos)
+      },
+    ]
+  },
   { path: 'animais', loadComponent: () => import('./pages/animais/animais').then(m => m.Animais) },
-  { path: 'responsaveis', loadComponent: () => import('./pages/tutor/tutor').then(m => m.Tutor) },
+  { path: 'admin/tutores', loadComponent: () => import('./pages/tutor/tutor').then(m => m.Tutor) },
   { path: 'services', loadComponent: () => import('./pages/servicos/servicos').then(m => m.Servicos) },
   { path: 'veterinarios', loadComponent: () => import('./pages/veterinarios/veterinarios').then(m => m.Veterinarios) },
 ];
