c34e21929b5e111ad18bd4e1e87f59161d17e84d
GiltMonster <lucassantoslukas2@gmail.com>
2025-11-10 02:46:32 -0300

    feat ✨: implementa o componente ModalEdit para edição de tutores, incluindo máscara de CPF e telefone, e integração com o serviço de usuário

--- front_end/prisma_pet/src/app/components/tutor-card/tutor-card.ts
+++ front_end/prisma_pet/src/app/components/tutor-card/tutor-card.ts
@@ -4,10 +4,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { UserInterface } from '../../interfaces';
 import { UsuarioService } from '../../services/usuario-service';
 import { FormatPhonePipe } from '../../pipes/format-phone-pipe';
+import { ModalEdit } from '../modal-edit/modal-edit';
 
 @Component({
   selector: 'app-tutor-card',
-  imports: [CommonModule, MatIconModule, FormatPhonePipe],
+  imports: [CommonModule, MatIconModule, FormatPhonePipe, ModalEdit],
   standalone: true,
   templateUrl: './tutor-card.html',
   styleUrls: ['./tutor-card.scss']
@@ -17,6 +18,10 @@ export class TutorCard implements OnInit {
   @Input() userTutor!: UserInterface;
   @Input() typeUser: string = '';
   @Output() tutorDeleted = new EventEmitter<number>();
+  @Output() edit = new EventEmitter<UserInterface>();
+
+  editModalOpen = false;
+  tutorToEdit?: UserInterface;
 
   constructor(
     private usuarioService: UsuarioService,
@@ -41,7 +46,24 @@ export class TutorCard implements OnInit {
     });
   }
 
-  editTutor(tutorId: number) { }
+  editTutor(tutorId: number) {
+    this.edit.emit(this.userTutor);
+  }
 
+  openEditModal(tutor: UserInterface) {
+    this.tutorToEdit = { ...tutor };
+    this.editModalOpen = true;
+  }
+
+  closeEditModal() {
+    this.editModalOpen = false;
+    this.tutorToEdit = undefined;
+  }
+
+  saveTutor(editedTutor: UserInterface) {
+    // Atualize a lista de tutores com os dados editados
+
+    this.closeEditModal();
+  }
 
 }
