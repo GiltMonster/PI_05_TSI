466e01ecf5da3ac096e268876082c1685c87f90d
GiltMonster <lucassantoslukas2@gmail.com>
2025-11-14 04:21:12 -0300

    feat ✨: aprimora componente HeaderPet com novas funcionalidades de navegação e exibição de detalhes dos pets; adiciona classes de estilo para botões desabilitados

--- front_end/prisma_pet/src/app/components/header-pet/header-pet.ts
+++ front_end/prisma_pet/src/app/components/header-pet/header-pet.ts
@@ -9,26 +9,44 @@ import { DatePipe } from '@angular/common';
   styleUrl: './header-pet.scss',
   standalone: true,
 })
-export class HeaderPet implements OnInit{
+export class HeaderPet implements OnInit {
 
   @Input() petsList: FichaPetInterface = { tutor_name: '', pets: [] };
   indexPet: number = 0;
 
-  constructor()
-  { }
+  class_btn_next: string = 'btn btn--primary';
+  class_btn_prev: string = 'btn btn--primary';
+
+  constructor() { }
 
   ngOnInit(): void {
 
   }
 
+  btnNextDisabled(): boolean {
+    return this.indexPet >= this.petsList.pets.length - 1;
+  }
+
+  btnPrevDisabled(): boolean {
+    return this.indexPet <= 0;
+  }
+
+  getNextBtnClass(): string {
+    return this.btnNextDisabled() ? 'btn btn--disabled' : 'btn btn--primary';
+  }
+
+  getPrevBtnClass(): string {
+    return this.btnPrevDisabled() ? 'btn btn--disabled' : 'btn btn--primary';
+  }
+
   onChangeNextPet() {
-    if (this.indexPet < this.petsList.pets.length - 1) {
+    if (!this.btnNextDisabled()) {
       this.indexPet++;
     }
   }
 
   onChangePrevPet() {
-    if (this.indexPet > 0) {
+    if (!this.btnPrevDisabled()) {
       this.indexPet--;
     }
   }
