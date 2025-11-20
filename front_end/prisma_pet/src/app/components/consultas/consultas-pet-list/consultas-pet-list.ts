1e1f7adb07c5153a508dc9c4ca115688f774b0a7
GiltMonster <lucassantoslukas2@gmail.com>
2025-11-20 17:16:47 -0300

    feat ✨: adiciona painel de expansão para exibir anamneses na listagem de consultas

--- front_end/prisma_pet/src/app/components/consultas/consultas-pet-list/consultas-pet-list.ts
+++ front_end/prisma_pet/src/app/components/consultas/consultas-pet-list/consultas-pet-list.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { PetConsulta } from '../../../interfaces';
 import { DatePipe } from '@angular/common';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-consultas-pet-list',
-  imports: [MatIconModule, DatePipe],
+  imports: [MatIconModule, DatePipe, MatExpansionModule],
   templateUrl: './consultas-pet-list.html',
   styleUrl: './consultas-pet-list.scss',
 })
