17d9972baaa59d25ba8236da91eb99436e1ea345
GiltMonster <lucassantoslukas2@gmail.com>
2025-11-24 01:55:43 -0300

    fix 🐞: remover importação não utilizada do validador no controlador de arquivos

--- back_end/app/Http/Controllers/FileController.php
+++ back_end/app/Http/Controllers/FileController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Validation\Validator;
 
 class FileController extends Controller
 {
